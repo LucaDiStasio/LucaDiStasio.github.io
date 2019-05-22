9e72dbd48b019d7f892a5d0d5033193635b815ab
Luca Di Stasio <luca.di.stasio@ltu.se>
2019-05-22 16:40:35 +0200

    commit on 22/05/2019 at 16:40:35

---
+++ assets/script/commonTools.js
@@ -0,0 +1,5 @@
+//=====================================================================================
+// Update copyright in footer
+//=====================================================================================
+var year = new Date().getFullYear();
+document.getElementById("copyright-p").innerHTML = '&copy; 2016 &ndash; ' + year + ' Luca Di Stasio';
